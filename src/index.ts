070906a0248d093d39a28798d8365e1e45a99833
danzin <excl.d.k@gmail.com>
2025-06-09 17:44:25 +0300

    removed redundand /src dir

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// import { Server } from "./server";
-// import { config } from "./utils/config";
-
-// const PORT = Number(process.env.PORT) || 3000;
-// const FRED_API_KEY = config.FRED_API_KEY!;
-
-// const server = new Server(PORT, FRED_API_KEY);
-// server.start();
--- src/services/messaging.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface MessageBroker {
-	connect(): Promise<void>;
-	publish(exchangeName: string, routingKey: string, message: any): Promise<void>;
-	consume(queueName: string, onMessage: (message: any) => Promise<void>): Promise<void>; // 'any' can be Signal later
-	close(): Promise<void>;
-}
