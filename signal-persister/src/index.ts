7b22658a9be081cb3845d942022885161786a523
danzin <excl.d.k@gmail.com>
2025-06-28 17:47:40 +0300

    adding comments

--- signal-persister/src/index.ts
+++ signal-persister/src/index.ts
@@ -12,6 +12,7 @@ async function main() {
 		process.exit(1);
 	}
 
+	// Initialize services
 	const rabbitMQService = new RabbitMQService(config.RABBITMQ_URL);
 	const timescaleDBService = new TimescaleDBService({
 		host: config.DB_HOST,
@@ -21,6 +22,7 @@ async function main() {
 		database: config.DB_NAME,
 	});
 
+	// Create the SignalProcessor instance with the services and queue/exchange names
 	const signalProcessor = new SignalProcessor(
 		rabbitMQService,
 		timescaleDBService,
