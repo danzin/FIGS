9b97a77c6286c7716e4a9d38e9fe9f74f968c46b
danzin <excl.d.k@gmail.com>
2025-07-19 19:50:31 +0300

    scraper working

--- scraper-service/src/models/Scraper.interface.ts
+++ scraper-service/src/models/Scraper.interface.ts
@@ -1,6 +1,5 @@
 import { MarketDataPoint, IndicatorDataPoint } from "@financialsignalsgatheringsystem/common";
 
-// A scraper can return data that fits into either of our main tables
 export type ScraperResult = IndicatorDataPoint | MarketDataPoint[] | null;
 
 export interface Scraper {
